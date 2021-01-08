caf8b082655dbfa5acba563bf611964100fba63f
NotEnding <zwzhengke@163.com>
2021-01-08 11:31:07 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+#-*- coding:utf-8 _*-
+"""
+@file: HelloWorld.py
+@time: 2021/01/05
+@site:
+@software: PyCharm
+
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
 import sys
 
 from maotai.jd_spider_requests import JdSeckill
